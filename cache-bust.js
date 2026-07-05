3c42b2f3a742c07f01f3154103f5bf56b8232b1a
happy333rich-crypto <happy333rich@gmail.com>
2026-07-05 22:05:34 +0800

    加入商品圖片快取更新機制

---
+++ cache-bust.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(() => {
+  const originalFetch = window.fetch.bind(window);
+  const version = "20260705-10";
+
+  window.fetch = (input, init = {}) => {
+    const rawUrl = typeof input === "string" ? input : input.url;
+    if (/^(products\.json|image-data\/)/.test(rawUrl)) {
+      const separator = rawUrl.includes("?") ? "&" : "?";
+      return originalFetch(`${rawUrl}${separator}v=${version}`, {
+        ...init,
+        cache: "reload"
+      });
+    }
+    return originalFetch(input, init);
+  };
+})();
