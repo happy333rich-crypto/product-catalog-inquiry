2fc4906ca97948ba2e2a0363b5be81dac1f965c3
happy333rich-crypto <happy333rich@gmail.com>
2026-07-06 12:52:15 +0800

    Refresh wet wipes catalog cache

--- cache-bust.js
+++ cache-bust.js
@@ -2,11 +2,11 @@
 
 (() => {
   const originalFetch = window.fetch.bind(window);
-  const version = "20260706-2";
+  const version = "20260706-4";
 
   window.fetch = (input, init = {}) => {
     const rawUrl = typeof input === "string" ? input : input.url;
-    if (/^(products\.json|image-data\/)/.test(rawUrl)) {
+    if (/^(products\.json|wet-wipes\.json|image-data\/)/.test(rawUrl)) {
       const separator = rawUrl.includes("?") ? "&" : "?";
       return originalFetch(`${rawUrl}${separator}v=${version}`, {
         ...init,
