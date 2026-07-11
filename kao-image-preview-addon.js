9454b34a43988a2b817466a9a67d323698dcf4d4
happy333rich-crypto <happy333rich@gmail.com>
2026-07-11 16:38:31 +0800

    Add first verified Kotex image to preview

---
+++ kao-image-preview-addon.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(() => {
+  const app = window.CatalogApp;
+  if (!app) return;
+
+  const imageOverrides = {
+    "608350": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_teatree/_teatreesuper_image1_1572_1152.jpg?rev=-1"
+  };
+
+  const originalInit = app.init;
+  app.init = async () => {
+    await originalInit();
+
+    app.state.products = app.state.products.map((product) =>
+      imageOverrides[product.id]
+        ? { ...product, image: imageOverrides[product.id] }
+        : product
+    );
+
+    app.applyFilters();
+    app.updateCartUI();
+  };
+})();
