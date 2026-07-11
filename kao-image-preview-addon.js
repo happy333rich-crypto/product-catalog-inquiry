3504a5d675d916e95bdb9f2f8e1e39ec44f9a3c3
happy333rich-crypto <happy333rich@gmail.com>
2026-07-11 22:16:03 +0800

    Correct Kleenex product list in preview

--- kao-image-preview-addon.js
+++ kao-image-preview-addon.js
@@ -8,15 +8,27 @@
   // 目前先撤下先前共用系列圖的錯誤配圖，避免不同尺寸顯示同一包裝。
   const imageOverrides = {};
 
+  const removedProductIds = new Set([
+    "843070", // 舒潔 ufufy 濕式面紙
+    "890180", // 舒潔食品級摺疊紙巾 150張×2入
+    "841870"  // 舒潔淨99抗菌濕巾
+  ]);
+
+  const productNameOverrides = {
+    "811700": "舒潔雲絨舒適抽取衛生紙"
+  };
+
   const originalInit = app.init;
   app.init = async () => {
     await originalInit();
 
-    app.state.products = app.state.products.map((product) =>
-      imageOverrides[product.id]
-        ? { ...product, image: imageOverrides[product.id] }
-        : product
-    );
+    app.state.products = app.state.products
+      .filter((product) => !removedProductIds.has(product.id))
+      .map((product) => ({
+        ...product,
+        name: productNameOverrides[product.id] || product.name,
+        image: imageOverrides[product.id] || product.image
+      }));
 
     app.applyFilters();
     app.updateCartUI();
