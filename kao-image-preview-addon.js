35e298314faca942f47bda393b5e452d5b895648
happy333rich-crypto <happy333rich@gmail.com>
2026-07-11 19:18:11 +0800

    Add official Kotex Soft series images for three products

--- kao-image-preview-addon.js
+++ kao-image-preview-addon.js
@@ -6,7 +6,10 @@
 
   const imageOverrides = {
     "608350": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_teatree/_teatreesuper_image1_1572_1152.jpg?rev=-1",
-    "608360": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_teatree/_teatreesuper_image1_1572_1152.jpg?rev=-1"
+    "608360": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_teatree/_teatreesuper_image1_1572_1152.jpg?rev=-1",
+    "608521": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_soft/kotex__soft_image1_1572_1152.jpg?rev=-1",
+    "608541": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_soft/kotex__soft_image1_1572_1152.jpg?rev=-1",
+    "608561": "https://www.kotex.com.tw/-/media/feature/kotex/apac/tw/product/pdp-page/_soft/kotex__soft_image1_1572_1152.jpg?rev=-1"
   };
 
   const originalInit = app.init;
