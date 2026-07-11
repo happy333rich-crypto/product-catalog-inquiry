77e22f969c1bbc831c06b96b03c2b574e792827a
happy333rich-crypto <happy333rich@gmail.com>
2026-07-11 15:58:07 +0800

    Group Kleenex-related catalog brands in preview

--- preview-brand-group.js
+++ preview-brand-group.js
@@ -4,18 +4,26 @@
   const app = window.CatalogApp;
   if (!app) return;
 
-  const groupedBrands = new Set(["居居加", "居美媞", "妙妙熊", "鉅瑋"]);
-  const groupedLabel = "居居加/居美媞/妙妙熊/鉅瑋";
+  const brandGroups = [
+    {
+      brands: new Set(["居居加", "居美媞", "妙妙熊", "鉅瑋"]),
+      label: "居居加/居美媞/妙妙熊/鉅瑋"
+    },
+    {
+      brands: new Set(["可麗舒", "可立雅"]),
+      label: "可麗舒/可立雅"
+    }
+  ];
+
   const originalInit = app.init;
 
   app.init = async () => {
     await originalInit();
 
-    app.state.products = app.state.products.map((product) =>
-      groupedBrands.has(product.brand)
-        ? { ...product, brand: groupedLabel }
-        : product
-    );
+    app.state.products = app.state.products.map((product) => {
+      const group = brandGroups.find(({ brands }) => brands.has(product.brand));
+      return group ? { ...product, brand: group.label } : product;
+    });
 
     [app.el.brand, app.el.category].forEach((select) => {
       while (select.options.length > 1) select.remove(1);
